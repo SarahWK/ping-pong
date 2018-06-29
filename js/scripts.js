7a026b96ecebdf2e5f574d36f9dfe034e4ecd148
SarahWK <sarahkulubya@gmail.com>
2018-06-29 16:41:23 +0300

    added my first if else statement for things divisible by 15

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//Business logic
+for var = y (y =1; y<= 100; y++){
+var userInput = "";
+if (y%3===0) = userInput + "Ping"
+if (y%5===0) = userInput + "Pong"
+if (y%15===0)= userInput + "PingPong"
+
+}
+
+var userInput=$("replies-Input").val()
+
+
+
+
+
+
+//UI Logic
+$(document).ready(function){
+  $("#replies-form").submit(function(event){
+    event.preventDefault()
+    $("#reply").append()
+  })
+}
