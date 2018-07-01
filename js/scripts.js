5138b5be713afc9e37083355f257d9c0d9e2c340
SarahWK <sarahkulubya@gmail.com>
2018-07-02 02:41:03 +0300

    fixed the errors in the code

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,19 @@ function pIngPong() {
   for (var i = 1; i <= 100; i++) {
           output = '';
           if ( i % 15 === 0 ) {
-            output+=‘PingPong’;//if the number is divisible by 15, print out a range and include PingPong
+            $('').append("<li>‘PingPong’</li>")//if the number is divisible by 15, print out a range and include PingPong
           }
-          else if ( i % 5 === 0 ) {
-              output+=‘Pong’;//if function is divisible by 5, print out a range and include Pong
+          else  ( i % 5 === 0 ) {
+          $('').append("<li>'Pong’</li>");//if function is divisible by 5, print out a range and include Pong
           }
           else if ( i % 3 ===  0 ) {
-               output+= ‘Ping’;//if function is divisible by 3, print out a range and include Ping
+          $('').append("<li>‘Ping’</li>");//if function is divisible by 3, print out a range and include Ping
           }
           else {
           return(i);
       }
     }
-    }
+  })
 
 
 event.preventDefault();
