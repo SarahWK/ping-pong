2e577745e444ee40733402974141b1d6041369b9
SarahWK <sarahkulubya@gmail.com>
2018-07-02 02:24:49 +0300

    Fixed the indenting and named the var

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 //Business logic
 // Input the PingPong function
-function PingPong() {
-for (var i = 1; i <= 100; i++) {
-  if ( i % 15 === 0 ) {
-    return(‘PingPong’);//if the number is divisible by 15, print out a range and include PingPong
-  }
-  else if ( i % 5 === 0 ) {
-    return(‘Pong’);//if function is divisible by 5, print out a range and include Pong
-  }
-  else if ( i % 3 ===  0 ) {
-    return(‘Ping’);//if function is divisible by 3, print out a range and include Ping
-  }
-  else {
-    return(i);
-  }
-}
-}
+function pIngPong() {
+  var output;
+  for (var i = 1; i <= 100; i++) {
+          output = '';
+          if ( i % 15 === 0 ) {
+            output+=‘PingPong’;//if the number is divisible by 15, print out a range and include PingPong
+          }
+          else if ( i % 5 === 0 ) {
+              output+=‘Pong’;//if function is divisible by 5, print out a range and include Pong
+          }
+          else if ( i % 3 ===  0 ) {
+               output+= ‘Ping’;//if function is divisible by 3, print out a range and include Ping
+          }
+          else {
+          return(i);
+      }
+    }
+    }
 
 
 event.preventDefault();
