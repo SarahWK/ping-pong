dfb0800c026d2db145c213cdc883c5bc8922d6d2
SarahWK <sarahkulubya@gmail.com>
2018-07-02 02:12:03 +0300

    Added comments to explain more about the function

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 //Business logic
-//Input the PingPong function
+// Input the PingPong function
 function PingPong() {
 for (var i = 1; i <= 100; i++) {
   if ( i % 15 === 0 ) {
-    console.log(‘PingPong’);//if the number is divisible by 15, print out a range and include PingPong
+    return(‘PingPong’);//if the number is divisible by 15, print out a range and include PingPong
   }
   else if ( i % 5 === 0 ) {
-    console.log(‘Pong’);//if function is divisible by 5, print out a range and include Pong
+    return(‘Pong’);//if function is divisible by 5, print out a range and include Pong
   }
   else if ( i % 3 ===  0 ) {
-    console.log(‘Ping’);
+    return(‘Ping’);//if function is divisible by 3, print out a range and include Ping
   }
   else {
-    console.log(i);
+    return(i);
   }
 }
+}
 
-return replies;
 
 event.preventDefault();
 
