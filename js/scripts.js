87e309934dd98d4b09aac5f82355bcc44b2ed9e1
SarahWK <sarahkulubya@gmail.com>
2018-07-09 11:23:52 +0300

    fixed an error in line 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,24 @@
 //Business logic
 // Input the PingPong function
-function pIngPong() {
-  var output;
-  for (var i = 1; i <= 100; i++) {
-          output = '';
-          if ( i % 15 === 0 ) {
-            $('').append("<li>‘PingPong’</li>")//if the number is divisible by 15, print out a range and include PingPong
-          }
-          else  ( i % 5 === 0 ) {
-          $('').append("<li>'Pong’</li>");//if function is divisible by 5, print out a range and include Pong
-          }
-          else if ( i % 3 ===  0 ) {
-          $('').append("<li>‘Ping’</li>");//if function is divisible by 3, print out a range and include Ping
-          }
-          else {
-          return(i);
-      }
-    }
-  })
-
-
-event.preventDefault();
-
-
+function pIngPong(i) {
+  if (i % 15 === 0){
+    return "PingPong" //if the number is divisible by 15, print out a range and include PingPong
+  } else if(i % 5 === 0){
+    return ("<li>'Pong’</li>"); //if function is divisible by 5, print out a range and include Pong
+  } else if (i % 3 === 0){
+    return ("<li>‘Ping’</li>"); //if function is divisible by 3, print out a range and include Ping
+  } else if {
+    return i;
+  }
+}
+}
 
 
 //UI Logic
-$(document).ready(function){
-  $("#replies-form").submit(function(event){
-    event.preventDefault()
-    $("#replies").append(<li>)
-  })
-}
+$(document).ready(function() {
+      $("form#replies-form").submit(function(event) {
+          event.preventDefault()
+          for (var i = 1; i <= 100; i++) {
+            $("ul#reply").append("<li>"+ pIngPong(i)+ "</li>");
+          })
+      })
